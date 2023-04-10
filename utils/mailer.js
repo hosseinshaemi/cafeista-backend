e8a4d573c43c90e2913a5deb9163f06a8eae96e8
AmirHosseinFathi <f.amirHossein81@gmail.com>
2023-04-09 20:21:30 -0700

    added a log in mailing in mailer.js

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,24 +1,30 @@
-const nodemailer = require('nodemailer');
-const smtpTransport = require('nodemailer-smtp-transport');
+const nodemailer = require("nodemailer");
+const smtpTransport = require("nodemailer-smtp-transport");
 
 const transporterDetails = smtpTransport({
-  service: 'gmail',
+  service: "gmail",
   secure: true,
   auth: {
-    user: process.env.EMAIL_USERNAME,
-    pass: process.env.EMAIL_PASSWORD,
+    user: "hoseinshaemi1394@gmail.com",
+    pass: "dlgtifowvplgcerg",
   },
 });
 
 const sendEmail = (email, fullname, subject, message) => {
   const transporter = nodemailer.createTransport(transporterDetails);
   const options = {
-    from: process.env.EMAIL_USERNAME,
+    from: "hoseinshaemi1394@gmail.com",
     to: email,
     subject: subject,
     html: `<h1>سلام ${fullname}</h1>\n<p>${message} کد تایید شما</p>`,
   };
-  transporter.sendMail(options);
+  transporter.sendMail(options, function (err, info) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(info);
+    }
+  });
 };
 
 module.exports = sendEmail;
