a3d8922d95475058f2a498d7d0666468eecfd8b8
AmirHossein <f.amirhosssein81@gmail.com>
2023-04-09 22:17:51 -0700

    password pattern fixed

--- models/validators/cafeAuthValidator.js
+++ models/validators/cafeAuthValidator.js
@@ -1,4 +1,6 @@
 const Joi = require('joi');
+const { joiPasswordExtendCore } = require('joi-password');
+const joiPassword = Joi.extend(joiPasswordExtendCore);
 const schema = Joi.object({
   firstname: Joi.string().required(),
   lastname: Joi.string().required(),
@@ -6,9 +8,29 @@ const schema = Joi.object({
     'string.email': 'ایمیل به صورت صحیح وارد نشده است',
     'any.required': 'ایمیل باید وارد شود',
   }),
-  password: Joi.string().min(6).required().messages({
+ /*  password: Joi.string().min(6).required().messages({
     'string.min': 'پسورد باید حداقل دارای شش کاراکتر باشد',
-  }),
+  }), */
+  password: joiPassword
+    .string()
+    .minOfSpecialCharacters(1)
+    .minOfLowercase(1)
+    .minOfUppercase(1)
+    .minOfNumeric(1)
+    .noWhiteSpaces()
+    .onlyLatinCharacters()
+    .required()
+    .messages({
+      'password.minOfSpecialCharacters':
+        'پسورد باید حداقل دارای یک حرف خاص باشد',
+      'password.minOfLowercaes': ' پسورد باید حداقل دارای یک حرف کوچک باشد',
+      'password.minOfUppercase': 'پسورد باید حداقل دارای یک حرف بزرگ باشد',
+      'password.minOfNumeric': 'پسورد باید حداقل شامل یک عدد باشد',
+      'password.noWhiteSpaces': 'پسورد نباید دارای فضای خالی باشد',
+      'password.onlyLatinCharacters':
+        'پسورد فقط باید شامل کاراکتر انگلیسی باشد',
+    }),
+
   phonenumber: Joi.string()
     .regex(/09(1[0-9]|3[1-9]|2[1-9])-?[0-9]{3}-?[0-9]{4}/)
     .required()
