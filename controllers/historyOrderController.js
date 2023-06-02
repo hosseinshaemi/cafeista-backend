c512b1d22fee531047810f06c469c919fc775563
AmirHosseinFathi <f.amirhosssein81@gmail.com>
2023-06-02 12:56:29 +0430

    getHistoryCompleting

--- controllers/historyOrderController.js
+++ controllers/historyOrderController.js
@@ -1,9 +1,23 @@
-const { Cafe, User, Comment, Category, Item,Order } = require('../models');
+const { Model } = require('sequelize');
+const { Cafe, User, Comment, Category, Item, Order } = require('../models');
 
 const getHistory = async (req, res) => {
   const email = req.body.email;
   try {
     const user = await User.findOne({ where: email });
-    const ordres = await Order.findAll({where: email},)
+    const orders = await Order.findAll({
+      include: [
+        { model: User, where: { id: user.id } },
+        { model: Cafe, attributes: 'cafename' },
+      ],
+      attributes: ['paymentCode', 'totalCost', 'createdAt'],
+    });
+    res.status(200).json({
+      msg: orders,
+    });
   } catch (error) {}
+};
+
+module.exports = {
+  getHistory,
 };
